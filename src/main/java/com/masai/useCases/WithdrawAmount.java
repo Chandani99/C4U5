4858c95807bc8d2dae9b18498908c8393c1d1117
Chandani99 <er.chandni99@gmail.com>
2022-07-18 15:31:06 +0530

    added

---
+++ src/main/java/com/masai/useCases/WithdrawAmount.java
@@ -0,0 +1,38 @@
+package com.masai.useCases;
+
+import java.util.Scanner;
+
+import com.masai.AccException.InsufficientBalance;
+import com.masai.AccException.InvalidAccount;
+import com.masai.AccountDao.AccountDao;
+import com.masai.AccountDao.AccountDaoImpl;
+import com.masai.EMUtility.Account;
+
+public class WithdrawAmount {
+
+	public static void main(String[] args) throws InsufficientBalance {
+		
+			AccountDao ad=new AccountDaoImpl();
+			Scanner sc=new Scanner (System.in);
+			
+			System.out.println("Enter Account Id");
+			int accId=sc.nextInt();
+			
+			System.out.println("Enter amount");
+			int amount=sc.nextInt();
+			try {
+				Account acc=ad.findById(accId);
+				int amount1=ad.withdraw(amount, acc);
+				
+				System.out.println(amount1);
+				
+			}
+			catch(InvalidAccount ia){
+				System.out.println("Account not found");
+			}
+			
+		
+
+	}
+
+}
